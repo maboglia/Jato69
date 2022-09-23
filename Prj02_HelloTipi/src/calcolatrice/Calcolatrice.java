e52a4d113f2383b50835d9bffa6d239ca850e5d4
maboglia <mauro.bogliaccino@gmail.com>
2022-09-23 11:01:06 +0200

    agg

--- Prj02_HelloTipi/src/calcolatrice/Calcolatrice.java
+++ Prj02_HelloTipi/src/calcolatrice/Calcolatrice.java
@@ -2,18 +2,20 @@ package calcolatrice;
 
 public class Calcolatrice {
 	
-	public static void main(String[] args) {
-		
-		int a = 12;
-		int b = 4;
-		
-		System.out.println("Addizione " + (a + b));
-		System.out.println("Sottrazione " + (a - b));
-		System.out.println("Moltiplicazione " + (a * b));
-		System.out.println("Divisione " + (a / b));
-		
-		
-	}
+
 	
+	public static double addizione(double a, double b) {
+		return a + b;
+	}
+	public static double sottrazione(double a, double b) {
+		return a - b;
+	}
+	public static double moltiplicazione(double a, double b) {
+		return a * b;
+	}
+	public static double divisione(double a, double b) {
+		return a / b;
+	}
+		
 
 }
---
+++ Prj02_HelloTipi/src/calcolatrice/CalcolatriceDemo.java
@@ -0,0 +1,31 @@
+package calcolatrice;
+
+//tutto quello che è fuori dal package in cui mi trovo deve essere importato
+import java.util.Scanner;
+
+public class CalcolatriceDemo {
+	public static void main(String[] args) {
+		
+		double a = chiediNumero();
+		double b = chiediNumero();
+		
+		//int risultato = addizione(5,9);
+		
+		System.out.println("Addizione: " + Calcolatrice.addizione(a,b));
+		System.out.println("Sottrazione " + Calcolatrice.sottrazione(a,b));
+		System.out.println("Moltiplicazione " + Calcolatrice.moltiplicazione(a,b));
+		System.out.println("Divisione " + Calcolatrice.divisione(a,b));
+		 
+		
+	}
+	
+	public static double chiediNumero() {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Inserisci un numero");
+		double numero = input.nextDouble();
+		return numero;
+		
+	}
+	
+}
