a7b53ea6ebc9e70dda3ef29ba62ca21b8c7faf9e
maboglia <mauro.bogliaccino@gmail.com>
2022-11-22 16:22:48 +0100

    agg

--- Prj28_Gestionale/src/main/java/controller/ClienteServlet.java
+++ Prj28_Gestionale/src/main/java/controller/ClienteServlet.java
@@ -1,12 +1,19 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Cliente;
 
 @WebServlet("/cliente")
 public class ClienteServlet extends HttpServlet {
@@ -20,7 +27,18 @@ public class ClienteServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setAttribute("elencoCliente",ctrl.getClienti());
-		req.getRequestDispatcher("cliente.jsp").forward(req, resp);
+
+		JSONArray arrayClienti = new JSONArray();
+		
+		for(Cliente c : ctrl.getClienti()) {
+			
+			JSONObject clienteJson = new JSONObject(c);
+			arrayClienti.put(clienteJson);
+		}
+		
+		resp.setContentType("application/json");
+		resp.getWriter().append(arrayClienti.toString());
+		//req.getRequestDispatcher("cliente.jsp").forward(req, resp);
 	}
 
 	@Override
